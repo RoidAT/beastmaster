0a66a807bc0b9a540176ce94b436184f92b86369
Rene Schuetz <rene6212@gmail.com>
2025-02-20 13:12:49 +0100

    intial commit

---
+++ Slime/Script/slime.gd
@@ -0,0 +1,16 @@
+class_name Slime extends CharacterBody2D
+
+
+const SPEED = 100.0
+
+func _ready():
+	pass
+	
+func _process(delta): 
+	pass
+	
+
+func _physics_process(delta: float) -> void:
+	
+
+	move_and_slide()
